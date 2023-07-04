125d702fcc32d26e1f9e6a4cd66558c49355f6a5
Maycon Wallas <111979915+mayconwallas@users.noreply.github.com>
2023-07-04 16:15:17 -0300

    Update data.js
    arrumando caminho dos links 
    
    Signed-off-by: Maycon Wallas <111979915+mayconwallas@users.noreply.github.com>

--- docs/src/components/data.js
+++ docs/src/components/data.js
@@ -4,27 +4,27 @@ export const projectData = [
 		title: 'Lista de Supermercado',
 		description:
 			'Uma pequena ajuda quando for ao supermercado, com opção de incluir, editar ou remover itens.',
-		gitHubLink: 'https://github.com/mayconwallas/Portfolio/tree/main/docs/projetos/ListadeSupermercado',
+		gitHubLink: 'https://github.com/mayconwallas/Portfolio/tree/main/docs/src/components/indexDosProjetos/projetos/ListadeSupermercado',
 	},
 	{
 		id: 2,
 		title: 'One Piece',
 		description:
 			'Um dos meus primeiros projetos que fiz enquanto estava aprendendo HTML e CSS',
-		gitHubLink: 'https://github.com/mayconwallas/Portfolio/tree/main/docs/projetos/onepiece',
+		gitHubLink: 'https://github.com/mayconwallas/Portfolio/tree/main/docs/src/components/indexDosProjetos/projetos/onepiece',
 	},
 	{
 		id: 3,
 		title: 'Praticando a Lógica',
 		description:
 			'Um pequeno jogo feito para passar o tempo, o objetivo e atravessar a rua sem ser atropelado, cuidado tem carros mais rápidos que outros.',
-		gitHubLink: 'https://github.com/mayconwallas/Portfolio/tree/main/docs/projetos/jogoLogica',
+		gitHubLink: 'https://github.com/mayconwallas/Portfolio/tree/main/docs/src/components/indexDosProjetos/projetos/jogoLogica',
 	},
 	{
 		id: 4,
 		title: 'Baerbearia Alura',
 		description:
 			'Um dos primeiros sites que fiz no meu aprendizado na carreira de Front-end.',
-		gitHubLink: 'https://github.com/mayconwallas/Portfolio/tree/main/docs/projetos/Barbearia%20Alura',
+		gitHubLink: 'https://github.com/mayconwallas/Portfolio/tree/main/docs/src/components/indexDosProjetos/projetos/Barbearia%20Alura',
 	},
 ];
